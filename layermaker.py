6d2cb40abeba7cd26bcd2f992504f52acf004d77
Eero Antikainen <eero.antikainen@helsinki.fi>
2023-05-02 11:04:57 +0300

    realised that identity matrix is the identity element of matrix multiplication so we dont need it the formulas

--- layermaker.py
+++ layermaker.py
@@ -40,7 +40,7 @@ with open(f"{name}.m","w") as file:
     file.write('end\n\n')
     file.write('fu = str2func(append("@(x)",input("What function would you like to evaluate? (use x as the variable and write the function within ')
     file.write("'')")
-    file.write('"));\n')
+    file.write('")));\n')
     file.write('y = x;\n\n')
     file.write('for ii =1:length(x);\n')
     file.write('    y(ii) = fu(x(ii));\n')
@@ -98,43 +98,28 @@ with open(f"{name}.m","w") as file:
         
     
     
-    file.write(f'% Derivatives hitting the activation fct\n')
-    for i in range(len(layers)+1):
-        file.write(f'    S{i+1} = eye(length(f{i+1}));\n')
-
+    file.write(f'% Derivatives hitting the activation fct are identity matrices so we can disregard them\n')
+    
     file.write(f'% Calculate the derivatives and update the weight matrices and biases\n')
 
     file.write(f"    DL = (f{i+1} - y)'; \n")
 
-    file.write(f'    Db{i+1} = DL * S{i+1};\n')
+    file.write(f'    Db{i+1} = DL;\n')
 
     file.write(f"    b{i+1} = b{i+1} - delta * Db{i+1}';\n")
 
     file.write(f'    DA{i+1} = (f{i} * Db{i+1});\n\n')
 
     for i in range(len(layers)-1):
-        file.write(f'    Db{len(layers)-i} = Db{len(layers)+1-i} * A{len(layers)+1-i} * S{len(layers)-i};\n')
+        file.write(f'    Db{len(layers)-i} = Db{len(layers)+1-i} * A{len(layers)+1-i};\n')
         file.write(f"    b{len(layers)-i} = b{len(layers)-i} - delta * Db{len(layers)-i}';\n")
         file.write(f"    A{len(layers)+1-i} = A{len(layers)+1-i} - delta * DA{len(layers)+1-i}';\n")
         file.write(f'    DA{len(layers)-i} = (f{len(layers)-1-i} * Db{len(layers)-i});\n\n')
-    file.write(f'    Db{len(layers)-i-1} = Db{len(layers)-i} * A{len(layers)-i} * S{len(layers)-i-1};\n')
+    file.write(f'    Db{len(layers)-i-1} = Db{len(layers)-i} * A{len(layers)-i};\n')
     file.write(f"    b{len(layers)-i-1} = b{len(layers)-i-1} - delta * Db{len(layers)-i-1}';\n")
     file.write(f"    A{len(layers)-i} = A{len(layers)-i} - delta * DA{len(layers)-i}';\n")
     file.write(f'    DA{len(layers)-i-1} = (x * Db{len(layers)-i-1});\n\n')
 
-# -
-# % First layer D_{b1} L, update b1 and A2
-
-#     Db1 = Db2 * A2 * S1;
-
-#     b1 = b1 - delta * Db1';
-   
-#     A2 = A2 - delta * DA2'; 
-
-# % First layer D_{A1} L, update A1
-
-#     DA1 = (x * Db1);
-# -    
     file.write(f"    A1 = A1 - delta * DA1'; \n")
 
     file.write(f'end')
